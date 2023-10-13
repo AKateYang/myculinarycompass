156c577f91b8b55442cf4f36fd1f6934e5d2f7b2
Ashley Yang <ashley.kate.yang@gmail.com>
2023-10-12 23:34:41 -0400

    commit 1

---
+++ frontend/src/components/Path.js
@@ -0,0 +1,13 @@
+const app_name = 'cop4331-9-19-2023-ed38a9ffe472'
+exports.buildPath = 
+function buildPath(route)
+{
+    if (process.env.NODE_ENV === 'production') 
+    {
+        return 'https://' + app_name +  '.herokuapp.com/' + route;
+    }
+    else
+    {        
+        return 'http://localhost:5000/' + route;
+    }
+}
