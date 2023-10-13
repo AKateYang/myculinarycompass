2933040305608e09a12998ed87528798d27b3cca
Ashley Yang <ashley.kate.yang@gmail.com>
2023-10-13 00:07:05 -0400

    basic structure

--- frontend/src/components/Path.js
+++ frontend/src/components/Path.js
@@ -1,13 +1,8 @@
-const app_name = 'cop4331-9-19-2023-ed38a9ffe472'
-exports.buildPath = 
-function buildPath(route)
-{
-    if (process.env.NODE_ENV === 'production') 
-    {
-        return 'https://' + app_name +  '.herokuapp.com/' + route;
-    }
-    else
-    {        
-        return 'http://localhost:5000/' + route;
-    }
-}
+const app_name = "myculinarycompass-0c8901cce626";
+exports.buildPath = function buildPath(route) {
+  if (process.env.NODE_ENV === "production") {
+    return "https://" + app_name + ".herokuapp.com/" + route;
+  } else {
+    return "http://localhost:5000/" + route;
+  }
+};
--- server.js
+++ server.js
@@ -12,117 +12,11 @@ app.set("port", process.env.PORT || 5000);
 app.use(cors());
 app.use(bodyParser.json());
 
-var cardList = [
-  "Roy Campanella",
-  "Paul Molitor",
-  "Tony Gwynn",
-  "Dennis Eckersley",
-  "Reggie Jackson",
-  "Gaylord Perry",
-  "Buck Leonard",
-  "Rollie Fingers",
-  "Charlie Gehringer",
-  "Wade Boggs",
-  "Carl Hubbell",
-  "Dave Winfield",
-  "Jackie Robinson",
-  "Ken Griffey, Jr.",
-  "Al Simmons",
-  "Chuck Klein",
-  "Mel Ott",
-  "Mark McGwire",
-  "Nolan Ryan",
-  "Ralph Kiner",
-  "Yogi Berra",
-  "Goose Goslin",
-  "Greg Maddux",
-  "Frankie Frisch",
-  "Ernie Banks",
-  "Ozzie Smith",
-  "Hank Greenberg",
-  "Kirby Puckett",
-  "Bob Feller",
-  "Dizzy Dean",
-  "Joe Jackson",
-  "Sam Crawford",
-  "Barry Bonds",
-  "Duke Snider",
-  "George Sisler",
-  "Ed Walsh",
-  "Tom Seaver",
-  "Willie Stargell",
-  "Bob Gibson",
-  "Brooks Robinson",
-  "Steve Carlton",
-  "Joe Medwick",
-  "Nap Lajoie",
-  "Cal Ripken, Jr.",
-  "Mike Schmidt",
-  "Eddie Murray",
-  "Tris Speaker",
-  "Al Kaline",
-  "Sandy Koufax",
-  "Willie Keeler",
-  "Pete Rose",
-  "Robin Roberts",
-  "Eddie Collins",
-  "Lefty Gomez",
-  "Lefty Grove",
-  "Carl Yastrzemski",
-  "Frank Robinson",
-  "Juan Marichal",
-  "Warren Spahn",
-  "Pie Traynor",
-  "Roberto Clemente",
-  "Harmon Killebrew",
-  "Satchel Paige",
-  "Eddie Plank",
-  "Josh Gibson",
-  "Oscar Charleston",
-  "Mickey Mantle",
-  "Cool Papa Bell",
-  "Johnny Bench",
-  "Mickey Cochrane",
-  "Jimmie Foxx",
-  "Jim Palmer",
-  "Cy Young",
-  "Eddie Mathews",
-  "Honus Wagner",
-  "Paul Waner",
-  "Grover Alexander",
-  "Rod Carew",
-  "Joe DiMaggio",
-  "Joe Morgan",
-  "Stan Musial",
-  "Bill Terry",
-  "Rogers Hornsby",
-  "Lou Brock",
-  "Ted Williams",
-  "Bill Dickey",
-  "Christy Mathewson",
-  "Willie McCovey",
-  "Lou Gehrig",
-  "George Brett",
-  "Hank Aaron",
-  "Harry Heilmann",
-  "Walter Johnson",
-  "Roger Clemens",
-  "Ty Cobb",
-  "Whitey Ford",
-  "Willie Mays",
-  "Rickey Henderson",
-  "Babe Ruth",
-];
-
-const MongoClient = require("mongodb").MongoClient;
+// Allows the use of .env
 require("dotenv").config();
-const url = process.env.MONGODB_URL;
 
-console.log(url);
-
-const client = new MongoClient(url);
-client.connect();
-
+///////////////////////////////////////////////////
+// For Api endpoints
 app.post("/api/addcard", async (req, res, next) => {
   // incoming: userId, color
   // outgoing: error
@@ -198,6 +92,8 @@ app.post("/api/searchcards", async (req, res, next) => {
   res.status(200).json(ret);
 });
 
+///////////////////////////////////////////////////
+// For Heroku deployment
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -221,6 +117,15 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+///////////////////////////////////////////////////
+// For MongoDB Auth
+const MongoClient = require("mongodb").MongoClient;
+const url = process.env.MONGODB_URL;
+const client = new MongoClient(url);
+client.connect();
+
+///////////////////////////////////////////////////
+// For PORT specification
 app.listen(PORT, () => {
   console.log("Server listening on port " + PORT);
 });
