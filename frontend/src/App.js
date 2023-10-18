88a3d597e9e3bcdadeb5ebec0a854ffd5b7b5d8c
Ashley Yang <ashley.kate.yang@gmail.com>
2023-10-18 10:04:30 -0400

    basic template for register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,14 @@ import CardPage from "./pages/CardPage";
 import LandingPage from "./pages/LandingPage";
 import SavedRecipes from "./pages/SavedRecipes";
 import SignupPage from "./pages/SignupPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/cards" element={<CardPage />} />
         <Route path="/savedRecipes" element={<SavedRecipes />} />
         <Route path="/landing" element={<LandingPage />} />
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,11 +1,20 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 import PageTitle from "../components/PageTitle";
 import Login from "../components/Login";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
+      <button
+        onClick={() => {
+          navigate("/register");
+        }}
+      >
+        Register
+      </button>
       <PageTitle />
       <Login />
     </div>
