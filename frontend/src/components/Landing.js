ebcfc09fdfedfbce89b71617025a34df0694226b
“alforde01“ <120608165+alforde01@users.noreply.github.com>
2023-10-18 02:06:09 -0400

    add font and additional css styling

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -5,22 +5,26 @@ export const Landing = () => {
   return (
     <div className="frame">
       <div className="div">
-        <div className="navigation">
-          <button className="text-wrapper">Login</button>
-          <div className="text-wrapper-2">Sign-Up</div>
-          <div className="text-wrapper-3">About Us</div>
+        <div className="header-container">
+          <div className="title">culinary compass.</div>
+          <div className="navigation">
+            <button className="button">Login</button>
+            <button className="button">Sign-Up</button>
+            <button className="button">About Us</button>
+          </div>
         </div>
-        <div className="title">culinary compass.</div>
-        <p className="tagline">
-          Lets Put A Good Tagline
-          <br />
-          here. It Could Work.
-        </p>
-        <p className="description">
-          We can put a more subtle description here. We can change around the
-          background into one that work better. This page could look like this
-          or maybe not..
-        </p>
+        <div className="content-wrapper">
+          <p className="tagline">
+            Lets Put A Good Tagline
+            <br />
+            here. It Could Work.
+          </p>
+          <p className="description">
+            We can put a more subtle description here. We can change around the
+            background into one that work better. This page could look like this
+            or maybe not..
+          </p>
+        </div>
       </div>
     </div>
   );
