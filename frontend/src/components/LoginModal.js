a935fdeed25f2083dbbac8b3bfcdd168d50d494f
Ashley Yang <ashley.kate.yang@gmail.com>
2023-10-25 19:50:37 -0400

    added functionality

--- frontend/src/components/LoginModal.js
+++ frontend/src/components/LoginModal.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/loginModal.css";
 import classNames from "classnames";
+import Login from "./Login";
 
 const LoginModal = ({ isOpen, onClose, className }) => {
   const handleBackgroundClick = (e) => {
@@ -9,6 +10,58 @@ const LoginModal = ({ isOpen, onClose, className }) => {
     }
   };
 
+  ///////////////////////////////////////////////////
+  // This section send the input to backend
+  const app_name = "myculinarycompass-0c8901cce626";
+  function buildPath(route) {
+    if (process.env.NODE_ENV === "production") {
+      return "https://" + app_name + ".herokuapp.com/" + route;
+    } else {
+      return "http://localhost:5000/" + route;
+    }
+  }
+
+  var loginName;
+  var loginPassword;
+
+  const [message, setMessage] = useState("");
+
+  const doLogin = async (event) => {
+    event.preventDefault();
+
+    var obj = { login: loginName.value, password: loginPassword.value };
+    var js = JSON.stringify(obj);
+
+    try {
+      const response = await fetch(buildPath("api/login"), {
+        method: "POST",
+        body: js,
+        headers: { "Content-Type": "application/json" },
+      });
+
+      var res = JSON.parse(await response.text());
+
+      if (res.id <= 0) {
+        setMessage("User/Password combination incorrect");
+      } else {
+        var user = {
+          firstName: res.firstName,
+          lastName: res.lastName,
+          id: res.id,
+        };
+        localStorage.setItem("user_data", JSON.stringify(user));
+
+        setMessage("");
+        window.location.href = "/cards";
+      }
+    } catch (e) {
+      alert(e.toString());
+      return;
+    }
+  };
+
+  ///////////////////////////////////////////////////
+  // This section contains the html/react/input form
   return (
     <div
       className={classNames("custom-modal", { open: isOpen }, className)}
@@ -18,7 +71,7 @@ const LoginModal = ({ isOpen, onClose, className }) => {
         <span className="custom-close-btn" onClick={onClose}>
           &times;
         </span>
-        <form>
+        <form onSubmit={doLogin}>
           <div className="custom-login-header">
             <h2 className="custom-login-title">Login</h2>
             <p className="custom-question">Don’t have an account?</p>
@@ -30,23 +83,30 @@ const LoginModal = ({ isOpen, onClose, className }) => {
               className="input-field"
               id="login-username"
               placeholder=" "
+              ref={(c) => (loginName = c)}
             />
             <label for="login-username">Username</label>
           </div>
           <div className="floating-label">
             <input
-              type="text"
+              type="password"
               className="input-field"
               id="login-password"
               placeholder=" "
+              ref={(c) => (loginPassword = c)}
             />
             <label for="login-password">Password</label>
           </div>
-          <button type="submit" className="custom-login-submit">
+          <button
+            type="submit"
+            className="custom-login-submit"
+            onClick={doLogin}
+          >
             Login
           </button>
           <button id="forgot">Forgot Password?</button>
         </form>
+        <span id="loginResult">{message}</span>
       </div>
     </div>
   );
