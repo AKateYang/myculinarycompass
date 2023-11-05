528be3c75805f74ce27993120a3b059deebfb209
Ashley Yang <ashley.kate.yang@gmail.com>
2023-11-05 15:11:41 -0500

    in the middle of reorg

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,15 +4,15 @@ import {
   getUserFriends,
   addRemoveFriend,
 } from "../controllers/users.js";
-import { verifyToken } from "../middleware/auth.js";
+import { verifyToken } from "../middleware/verifytoken.js";
 
 const router = express.Router();
 
-/* READ */
-router.get("/:id", verifyToken, getUser);
-router.get("/:id/friends", verifyToken, getUserFriends);
+// /* READ */
+// router.get("/:id", verifyToken, getUser);
+// router.get("/:id/friends", verifyToken, getUserFriends);
 
-/* UPDATE */
-router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+// /* UPDATE */
+// router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,11 @@ import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
 // import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/verifytoken.js";
+// import User from "./data-models/User.js";
+// import Post from "./data-models/Post.js";
+// import { users, posts } from "./data/index.js";
 
-///////////////////////////////////////////////////
-// Configurations
+/* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -31,18 +33,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const path = require("path");
-// const PORT = process.env.PORT || 5000;
-// app.set("port", process.env.PORT || 5000);
-// app.use(bodyParser.json());
-// // Allows the use of .env
-// require("dotenv").config();
-
-///////////////////////////////////////////////////
-// FILE STORAGE
+/* FILE STORAGE */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/assets");
@@ -53,66 +44,27 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-///////////////////////////////////////////////////
-// ROUTES WITH FILES
+/* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
 // app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
-///////////////////////////////////////////////////
-// ROUTES
+/* ROUTES */
 app.use("/auth", authRoutes);
 // app.use("/users", userRoutes);
 // app.use("/posts", postRoutes);
 
-///////////////////////////////////////////////////
-// For MongoDB Auth
-const PORT = process.env.PORT || 5000;
-const MongoClient = require("mongodb").MongoClient;
-const ObjectId = require("mongodb").ObjectId;
-const url = process.env.MONGODB_URL;
-const client = new MongoClient(url);
-client.connect();
-// mongoose
-//   .connect(process.env.MONGODB_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-//     /* ADD DATA ONE TIME */
-//     // User.insertMany(users);
-//     // Post.insertMany(posts);
-//   })
-//   .catch((error) => console.log(`${error} did not connect`));
-
-///////////////////////////////////////////////////
-// For Heroku deployment
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
-  );
-  next();
-});
-
-// Server static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("frontend/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
-
-///////////////////////////////////////////////////
-// For PORT specification
-app.listen(PORT, () => {
-  console.log("Server listening on port " + PORT);
-});
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
