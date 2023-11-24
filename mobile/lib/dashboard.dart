b932afb845977a85f527fd01429150e2750db97e
Ashley Yang <ashley.kate.yang@gmail.com>
2023-11-23 23:58:06 -0500

    added nav bar mobile

--- mobile/lib/dashboard.dart
+++ mobile/lib/dashboard.dart
@@ -1,50 +1,95 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:mobile/pages/newfeed.dart';
+import 'package:mobile/pages/profile.dart';
+import 'package:mobile/pages/saved.dart';
 
 import 'utils/helper.dart';
 
-class DashboardPage extends StatelessWidget {
+class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  int _currentIndex = 0;
+
+  void _navigate(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  // All the pages
+  final List _pages = [
+    //dashboard
+    ProfilePage(),
+
+    NewfeedPage(),
+
+    SavedRecipes(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        title: const Text(
-          "Dashboard",
-          style: TextStyle(
-              fontSize: 28, fontWeight: FontWeight.bold, color: Colors.black),
-        ),
-        centerTitle: true,
-        systemOverlayStyle: SystemUiOverlayStyle.dark,
-      ),
-      body: SafeArea(
-        child: FutureBuilder(
-          future: getUserProfile(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(snapshot.data!.user.firstName,
-                        style: const TextStyle(fontSize: 24)),
-                    Text(snapshot.data!.user.lastName,
-                        style: const TextStyle(fontSize: 24)),
-                  ],
-                ),
-              );
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
-        ),
-      ),
-    );
+        backgroundColor: Colors.transparent,
+        // appBar: AppBar(
+        //   elevation: 0,
+        //   backgroundColor: Colors.white,
+        //   title: const Text(
+        //     "My Culinary Compass",
+        //     style: TextStyle(
+        //         fontSize: 28, fontWeight: FontWeight.bold, color: Colors.black),
+        //   ),
+        //   centerTitle: true,
+        //   systemOverlayStyle: SystemUiOverlayStyle.dark,
+        // ),
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _currentIndex,
+            onTap: _navigate,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: "Profile",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.dashboard_rounded),
+                label: "Dashboard",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.food_bank_rounded),
+                label: "SavedRecipe",
+              ),
+            ]),
+        body: _pages[_currentIndex]
+        // This is an example that displays the stored name using a Future Builder
+        // body: SafeArea(
+        //   child: FutureBuilder(
+        //     future: getUserProfile(),
+        //     builder: (context, snapshot) {
+        //       if (snapshot.hasData) {
+        //         return Center(
+        //           child: Column(
+        //             mainAxisAlignment: MainAxisAlignment.center,
+        //             children: [
+        //               Text(snapshot.data!.user.firstName,
+        //                   style: const TextStyle(fontSize: 24)),
+        //               Text(snapshot.data!.user.lastName,
+        //                   style: const TextStyle(fontSize: 24)),
+        //             ],
+        //           ),
+        //         );
+        //       } else {
+        //         return const Center(
+        //           child: CircularProgressIndicator(),
+        //         );
+        //       }
+        //     },
+        //   ),
+        // ),
+        );
   }
 }
---
+++ mobile/lib/pages/saved.dart
@@ -0,0 +1,213 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:mobile/widgets/app_color.dart';
+//import 'package:food_recipe/feature/home/model/recipe_model.dart';
+//import 'package:food_recipe/feature/home/presentation/widget/recipe_item.dart';
+
+class SavedRecipes extends StatelessWidget {
+  const SavedRecipes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.backgroundBlack,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top + 24,
+          bottom: MediaQuery.of(context).padding.bottom + 24,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Hello,",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge
+                        ?.copyWith(color: AppColor.primaryColor),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      // Navigate to grocery list
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      child: Image.asset(
+                        "assets/shopping_cart.png",
+                        width: 25, // Adjust the width as needed
+                        height: 25, // Adjust the height as needed
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 1),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Text(
+                "What you want to cook today ?",
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium
+                    ?.copyWith(color: AppColor.white),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(16),
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: AppColor.backgroundGray,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Row(
+                children: [
+                  //const Icon(Icons.search),
+                  const Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: TextField(
+                      style: TextStyle(
+                        color: AppColor.blackGrey,
+                        fontSize: 17,
+                      ),
+                      decoration: InputDecoration(
+                        hintText: "Search Recipes",
+                        hintStyle: Theme.of(context)
+                            .textTheme
+                            .bodyMedium
+                            ?.copyWith(color: AppColor.blackGrey),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // Container(
+            //   margin: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+            //   padding: const EdgeInsets.fromLTRB(16, 10, 16, 0),
+            //   decoration: BoxDecoration(
+            //     color: AppColor.primaryColor.withOpacity(0.3),
+            //     borderRadius: BorderRadius.circular(16),
+            //   ),
+            //   child: Row(
+            //     crossAxisAlignment: CrossAxisAlignment.start,
+            //     children: [
+            //       //const Icon(Icons.info_outline),
+            //       const SizedBox(width: 8),
+            //       Expanded(
+            //         child: Column(
+            //           crossAxisAlignment: CrossAxisAlignment.start,
+            //           children: [
+            //             Text(
+            //               "New Recipes the Categorires you are subscribed.",
+            //               style: Theme.of(context).textTheme.bodyMedium,
+            //             ),
+            //             TextButton(
+            //               onPressed: () {},
+            //               child: Text(
+            //                 "See Recipes",
+            //                 style: Theme.of(context).textTheme.button?.copyWith(
+            //                       color: AppColor.primaryColor,
+            //                       decoration: TextDecoration.underline,
+            //                     ),
+            //               ),
+            //             )
+            //           ],
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Suggested Recipes",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium
+                        ?.copyWith(color: AppColor.white),
+                  ),
+                  // TextButton(
+                  //   onPressed: () {},
+                  //   child: const Text("See All"),
+                  // )
+                ],
+              ),
+            ),
+            const SizedBox(height: 15),
+            SizedBox(
+              height: 180,
+              // child: ListView.separated(
+              //   itemCount: latestRecipes.length,
+              //   scrollDirection: Axis.horizontal,
+              //   padding: const EdgeInsets.symmetric(horizontal: 24),
+              //   separatorBuilder: (_, __) {
+              //     return const SizedBox(width: 13);
+              //   },
+              //   itemBuilder: (context, index) {
+              //     final recipe = latestRecipes[index];
+              //     return RecipeItem(recipe: recipe);
+              //   },
+              // ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Saved Recipes",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium
+                        ?.copyWith(color: AppColor.white),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(""),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 8),
+            SizedBox(
+              height: 340,
+              child: GridView.count(
+                //itemCount: trandingRecipes.length,
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                scrollDirection: Axis.vertical,
+                // semanticChildCount: trandingRecipes.length,
+                // padding: const EdgeInsets.symmetric(horizontal: 24),
+                // children: List.generate(trandingRecipes.length,
+                //     (index) => RecipeItem(recipe: trandingRecipes[index])),
+                // separatorBuilder: (_, __) {
+                //   return const SizedBox(width: 16);
+                // },
+                // itemBuilder: (context, index) {
+                //   final recipe = trandingRecipes[index];
+                //   return RecipeItem(recipe: recipe);
+                // },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
