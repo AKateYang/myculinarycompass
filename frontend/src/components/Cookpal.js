d495fcbe99475986352658d56a0714cc932c77d3
serinachugani <63365660+serinachugani@users.noreply.github.com>
2023-11-27 03:03:09 -0500

    saved recipes page

--- frontend/src/components/Cookpal.js
+++ frontend/src/components/Cookpal.js
@@ -1,134 +1,199 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./css/SavedRecipes.css";
 
 const Cookpal = () => {
-  return (
-    <div className="cookpal">
+
+  const [recipes, setRecipes] = useState([]);
+  var _ud = localStorage.getItem("user_data");
+  var ud = JSON.parse(_ud);
+  var userId = ud.id;
+
+  useEffect(() => {
+    // Replace 'API_ENDPOINT' with your actual endpoint URL
+    var bp = require("./Path.js");
+    fetch(bp.buildPath("recipes/getLazyLoadingRecipes"))
+      .then(response => response.json())
+      .then(data => {
+        console.log('Fetched data:', data);  // This line logs the fetched data
+        setRecipes(data);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  // const handleSaveorUnsave = async () => {
+  //   try {
+  //     var bp = require("./Path.js");
+  //     fetch(bp.buildPath("recipes/saveAndUnsaveRecipes"))
+  //       .then(response => response.json())
+  //       .then(data => {
+  //         console.log('Fetched data:', data);
+  //       })
+  //     const updatedRecipe = await response.json();
+  //     console.log('Updated Recipe:', updatedRecipe);
+  //   } catch (error) {
+  //     console.error('There was a proble:', error);
+  //   }
+  // };
+  
+  // useEffect(() => {
+  //   // Replace 'API_ENDPOINT' with your actual endpoint URL
+  //   var bp = require("./Path.js");
+  //   fetch(bp.buildPath("recipes/saveRecipe/:userId/:recipeId"))
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       console.log('Fetched data:', data);  // This line logs the fetched data
+  //       setRecipes(data);
+  //     })
+  //     .catch(error => console.error('Error fetching data:', error));
+  // }, []);
+
+  const doLogout = event => 
+    {
+	    event.preventDefault();
+
+        localStorage.removeItem("user_data")
+        window.location.href = '/';
+
+    };
+
+  function changeButton(index, newText, newColor, userId, recipeId)
+  {
+    const divsWithKey = document.querySelectorAll(`div[data-key="${index}"]`);
+
+    divsWithKey.forEach(div => {
+      const button = div.querySelector('button'); // Get the button inside the div
+
+      if (button) {
+        button.addEventListener('click', function() {
+            button.textContent = newText;
+            button.style.backgroundColor = newColor;
+            //saveRecipe(userId, recipeId);
+            var bp = require("./Path.js");
+            fetch(bp.buildPath("recipes/saveRecipe/" + `${userId}/${recipeId}`), {
+              method: 'PATCH',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              // If you need to send a body with the POST request, add it here
+              // body: JSON.stringify({ /* data */ })
+            })
+            .then(response => response.json())
+            .then(data => {
+              console.log('Fetched data:', data);
+            })
+            .catch(error => console.error('Error fetching data:', error));
+        });
+      }
+    });
+  };
+
+  // function getPicture(index)
+  // {
+  //   const divsWithKey = document.querySelectorAll(`div[data-key="${index}"]`);
+
+  //   divsWithKey.forEach(div => {
+  //     const img = div.querySelector('img'); // Get the button inside the div
+
+  //     if (img) {
+  //       const picturePath = "./images/food-" + (index + 1) + ".jpeg";
+  //       console.log(picturePath);
+  //       return picturePath;
+  //     }
+  //   });
+  // };
+
+    return (
+        <div className="cookpal">
       <div className="div">
         <div className="overlap">
-          <div className="text-wrapper">Saved Recipes</div>
-          <div className="text-wrapper-2">My Recipes</div>
-          <div className="text-wrapper-3">News Feed</div>
+          <div className="text-wrapper">
+            <button className="logout-button" >Saved Recipes</button>
+          </div>
+          <div className="text-wrapper-2">
+            <button className="logout-button" >My Recipes</button>
+          </div>
+          <div className="text-wrapper-3">
+            <button className="logout-button" >NewsFeed</button>
+          </div>
           <div className="overlap-group">
-            <div className="text-wrapper-4">Logout</div>
-            <div className="text-wrapper-5"></div>
+              <button className="logout-button" onClick={doLogout}>Log Out</button>
           </div>
         </div>
         <div className="overlap-2">
           <div className="overlap-3">
-            <p className="culinary-compass">
+            <div className="culinary-compass">
               <span className="span">
                 Culinary
                 <br />
               </span>
               <span className="text-wrapper-6">Compass</span>
-            </p>
+            </div>
             <div className="overlap-4">
-              <div className="div-wrapper">
-                <div className="text-wrapper-7">All Categories</div>
+              <div className="text-wrapper-8">
+                {/* <label for="textInput">Search: </label> */}
+                <input type="text" border="none" name="textInput"></input>
               </div>
-              <div className="text-wrapper-8">Search</div>
               <div className="overlap-5">
-                <div className="text-wrapper-9"></div>
+                <img className="text-wrapper-9" />
               </div>
             </div>
             <div className="overlap-6">
-              <div className="text-wrapper-10"></div>
-              <img
-                className="cropped-pistaciafood"
-                alt="Cropped pistaciafood"
-                src="cropped-pistaciafood-11-1.png"
-              />
-              <p className="get-cooking">
-                <span className="text-wrapper-11">Get&nbsp;&nbsp;</span>
-                <span className="text-wrapper-12">Cooking!</span>
-              </p>
+              <div className="get-cooking-bg">
+                <p className="get-cooking">
+                  <span className="text-wrapper-11">Get&nbsp;&nbsp;</span>
+                  <span className="text-wrapper-12">Cooking!</span>
+                </p>
+              </div>
             </div>
-            <img className="ellipse" alt="Ellipse" src="ellipse-6.png" />
-          </div>
-          <div className="overlap-7">
-            <div className="ellipse-2" />
-            <div className="text-wrapper-13">3</div>
           </div>
         </div>
         <div className="overlap-8">
           <div className="overlap-9">
-            <div className="overlap-10">
-              <img className="image" alt="Image" src="image-2.png" />
-              <img
-                className="rectangle"
-                alt="Rectangle"
-                src="rectangle-48.png"
-              />
-              <div className="group">
-                <div className="overlap-group-2">
-                  <div className="text-wrapper-18">Recipes</div>
-                  <div className="text-wrapper-19">Winter salads</div>
-                  <div className="text-wrapper-20">Organic chicken</div>
-                </div>
-                <div className="text-wrapper-21">Beef and Mutton</div>
-                <div className="text-wrapper-22">Flavoured Milk</div>
-              </div>
-            </div>
-            <div className="group-2">
-              <img className="img" alt="Rectangle" src="rectangle-27.png" />
-              <img
-                className="rectangle-2"
-                alt="Rectangle"
-                src="rectangle-28.png"
-              />
-              <img
-                className="rectangle-3"
-                alt="Rectangle"
-                src="rectangle-29.png"
-              />
-            </div>
-            <img className="chat" alt="Chat" src="chat-4.png" />
-            <img className="chat-2" alt="Chat" src="chat-5.png" />
-            <img className="chat-3" alt="Chat" src="chat-6.png" />
-            <div className="group-3">
+          <div className="suggested-recipes-title">Suggested Recipes</div>
+              {recipes.map((recipe, index) => (
+                  <div data-key={index} key={index} className="group-2">
+                    <img className="img" src="./images/food-1.jpeg" alt={recipe.name} />
+                    <button id="addbutton" className="frame-6-1" onClick={changeButton(index, "Added", "#F05E1633", userId, recipe._id)}>Add</button>
+                    <div id="recipeName" className="recipe-name">{recipe.recipeName}</div>
+                    <div className="time-image"></div>
+                    <div className="time-length">{recipe.time}</div>
+                    <div className="recipe-likes-img" />
+                    <div className="comments-img" />
+                    <div className="recipe-likes">{recipe.likes}</div>
+                    <div className="recipe-comments">{recipe.comments}</div>
+                  </div>
+              ))}
+            {/* <div className="group-3">
               <div className="text-wrapper-32">FILTERS</div>
               <div className="text-wrapper-33">Diet</div>
+              <div className="filter-group">
+                <button className="filter-button">Dairy Free</button>
+                <button className="filter-button">Egg Free</button>
+                <button className="filter-button">Sugar Free</button>
+                <button className="filter-button">Gluten Free</button>
+              </div>
+              <div className="text-wrapper-33-1">Allergies</div>
+              <div className="filter-group">
+                <button className="filter-button">Nuts</button>
+                <button className="filter-button">Legumes</button>
+                <button className="filter-button">Grain</button>
+                <button className="filter-button">Fruit</button>
+              </div>
               <div className="text-wrapper-34">Cusine</div>
-              <div className="text-wrapper-35">Goals</div>
-              <div className="text-wrapper-36">Dairy Free</div>
-              <div className="text-wrapper-37">Asian</div>
-              <div className="text-wrapper-38">Weight loss</div>
-              <div className="text-wrapper-39">Egg Free</div>
-              <div className="text-wrapper-40">Italian</div>
-              <div className="text-wrapper-41">Freshness</div>
-              <div className="text-wrapper-42">Sugar Free</div>
-              <div className="text-wrapper-43">Chinese</div>
-              <div className="text-wrapper-44">Activeness</div>
-              <div className="text-wrapper-45">Gluten Free</div>
-              <div className="frame">
-                <div className="text-wrapper-46">Glutten</div>
-                <div className="text-wrapper-47">Legumes</div>
-                <div className="text-wrapper-47">Grain</div>
-                <div className="text-wrapper-47">Fruite</div>
-              </div>
-              <div className="text-wrapper-48">Thai</div>
-              <div className="text-wrapper-49">Rich Nutritions</div>
-              <div className="frame-2">
-                <div className="text-wrapper-50">Allergies</div>
-              </div>
-            </div>
-            <div className="frame-3" />
-            <div className="frame-4" />
-            <div className="frame-5" />
+              <div className="filter-group">
+                <button className="filter-button">Asian</button>
+                <button className="filter-button">Italian</button>
+                <button className="filter-button">Chinese</button>
+                <button className="filter-button">Thai</button>
+              </div>  
+            </div> */}
+            <div className="saved-recipes-title">Saved Recipes</div>
           </div>
-          <img className="line" alt="Line" src="line-2.svg" />
-          <img className="chat-4" alt="Chat" src="chat-1.png" />
-          <img className="chat-5" alt="Chat" src="chat-2.png" />
-          <img className="chat-6" alt="Chat" src="chat-3.png" />
-          <div className="rectangle-4" />
-          <div className="frame-6" />
-          <div className="frame-7" />
-          <div className="frame-8" />
         </div>
       </div>
     </div>
-  );
-};
+    
+    );
+  };
 
-export default Cookpal;
+  export default Cookpal;
