11a52ed63eb8a93506d72f8d521a7d345d3c05af
Ashley Yang <ashley.kate.yang@gmail.com>
2023-11-28 18:04:43 -0500

    testing server

--- frontend/src/components/Path.js
+++ frontend/src/components/Path.js
@@ -1,5 +1,5 @@
-const app_name = "myculinarycompass-0c8901cce626";
-// const app_name = "www.myculinarycompass.com";
+// const app_name = "myculinarycompass-0c8901cce626";
+const app_name = "www.myculinarycompass";
 exports.buildPath = function buildPath(route) {
   if (process.env.NODE_ENV === "production") {
     return "https://" + app_name + ".herokuapp.com/" + route;
