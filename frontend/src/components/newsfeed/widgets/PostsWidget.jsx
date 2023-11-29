11030fbde1fd0ec65f8eeb6785f7489bf4b395ca
Ahal Saias <109298048+Ahal122@users.noreply.github.com>
2023-11-29 07:07:45 -0500

    Fixed post retrival for homepage web

--- frontend/src/components/newsfeed/widgets/PostsWidget.jsx
+++ frontend/src/components/newsfeed/widgets/PostsWidget.jsx
@@ -6,9 +6,32 @@ import PostWidget from "./PostWidget";
 const PostsWidget = ({ isProfile, userId }) => {
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.posts);
+  const _uid = JSON.parse(localStorage.getItem("user_data"))._id;
+  var userId = _uid._id;
+
+  const serverBaseUrl = "https://www.myculinarycompass.com/";
 
   // Fetch posts
   useEffect(() => {
+    const getPosts = async () => {
+      const response = await fetch((serverBaseUrl+"posts/"), {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await response.json();
+      dispatch(setPosts({ posts: data }));
+      console.log({ posts: data });
+    };
+  
+    // UPDATED getUserPosts
+    const getUserPosts = async () => {
+      const response = await fetch((serverBaseUrl+ `posts/getPost/${userId}`), {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await response.json();
+      dispatch(setPosts({ posts: data }));
+    };
     // Define your fetch logic here based on 'isProfile' and 'userId'
     // Example: fetchPosts(isProfile ? `posts/user/${userId}` : 'posts/')
   }, [isProfile, userId, dispatch]);
